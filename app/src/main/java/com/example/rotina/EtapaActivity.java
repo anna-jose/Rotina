a487ce886cdb393f779bd819fd16f9af57f8608f
anna-jose <anna.jose@fatec.sp.gov.br>
2020-11-25 09:29:43 -0300

    Informação das etapas OK Próximo passo: timer e variável local

--- app/src/main/java/com/example/rotina/EtapaActivity.java
+++ app/src/main/java/com/example/rotina/EtapaActivity.java
@@ -58,19 +58,37 @@ public class EtapaActivity extends AppCompatActivity {
     }
 
     public void carregaEtapa(){
-        if (contador < 0 || contador > 4){ //funcionou ;-;
+        if (contador < 0 || contador > 6){ //funcionou ;-;
             Intent voltar = new Intent(EtapaActivity.this, MainActivity.class);
             startActivity(voltar);
         }else if (contador == 0){
-            txEtapa.setText("Etapa 1");
+            txEtapa.setText("Etapa 1 - Joelhos");
+            imIlustra.setImageResource(R.drawable.joelho);
+            txDescricao.setText("Deitado, coloque um calcanhar quase em cima do joelho da outra perna, ainda visível à você. \n\nSegure esta perna pelo outro lado e fique assim por 20 segundos.");
         }else if (contador == 1){
-            txEtapa.setText("Etapa 2");
+            txEtapa.setText("Etapa 2 - Coluna 1");
+            imIlustra.setImageResource(R.drawable.coluna1);
+            txDescricao.setText("Torça uma perna sobre a outra. Mantenha a coluna reta e braços abertos. \n\nAguarde 20 segundos.");
         }else if (contador == 2){
-            txEtapa.setText("Etapa 3");
+            txEtapa.setText("Etapa 3 - Coluna 2");
+            imIlustra.setImageResource(R.drawable.coluna2a);
+            txDescricao.setText("Vire de barriga para baixo, e empurre com as duas mãos, braços esticados e quadril deitado.");
         }else if (contador == 3){
-            txEtapa.setText("Etapa 4");
+            txEtapa.setText("Etapa 4 - Coluna 3");
+            imIlustra.setImageResource(R.drawable.coluna2b);
+            txDescricao.setText(" Agora, dobre os joelhos e deite sobre eles, apoiando a cabeça na cama.");
         }else if (contador == 4){
-            txEtapa.setText("Etapa 5 - FINAL");
+            txEtapa.setText("Etapa 5 - Braços 1");
+            imIlustra.setImageResource(R.drawable.bracos1);
+            txDescricao.setText("Sente e cruze as pernas. Apoie um braço do seu lado e alongue o outro para a mesma direção. \n\nMantenha o braço alongado no ar. ");
+        }else if (contador == 5){
+            txEtapa.setText("Etapa 6 - Pescoço");
+            imIlustra.setImageResource(R.drawable.pescoco);
+            txDescricao.setText("Relaxe e gire a cabeça em movimentos circulares, cuidadosamente. Tente o sentido horário e antihorário!");
+        }else if (contador == 6){
+            txEtapa.setText("Etapa 7 - Braços 2");
+            imIlustra.setImageResource(R.drawable.bracos2);
+            txDescricao.setText("Estique os braços acima da cabeça, entrelace os dedos por 20 segundos.");
         }
     }
 }
